2d2793cfd0f0cb9710bd674aa80aaa06b19297cf
Joking Taremba <jokingtaremba@gmail.com>
2024-02-20 16:45:30 +0200

    springBoot project

---
+++ src/main/java/com/jokingwill/todolistsimple/TodolistsimpleApplication.java
@@ -0,0 +1,13 @@
+package com.jokingwill.todolistsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistsimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistsimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jokingwill/todolistsimple/TodolistsimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jokingwill.todolistsimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistsimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
